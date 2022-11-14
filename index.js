935bbaeb63dc4b05f99385450add890f0d2a75d5
Gurkirat Singh <mr.gurkiratsing@gmail.com>
2022-11-14 14:38:57 +0530

    project completed

---
+++ index.js
@@ -0,0 +1,172 @@
+// SIDEBAR
+const menuItems= document.querySelectorAll('.menu-item');
+
+// MESSAGES
+const messagesNotification=document.querySelector('#messages-notification');
+const messages=document.querySelector('.messages');
+const message=messages.querySelectorAll('.message');
+const messageSearch=document.querySelector('#message-search');
+
+// THEME
+const theme= document.querySelector('#theme');
+const themeModal=document.querySelector('.customize-theme');
+const fontSizes=document.querySelectorAll('.choose-size span');
+var root=document.querySelector(':root');
+const colorPalette=document.querySelectorAll('.choose-color span');
+
+
+// ================== SIDEBAR ====================== 
+
+// remove active class from all items
+const changeActiveItem=()=>{
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+
+menuItems.forEach(item =>{
+    item.addEventListener('click',()=>{
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id!='notifications'){
+            document.querySelector('.notifications-popup').
+            style.display='none';
+        }else{
+            document.querySelector('.notifications-popup').
+            style.display='block';
+            document.querySelector('#notifications.notification-count').style.display='none';
+
+        }
+    })
+})
+
+
+
+
+// ====================== messages =======================
+// searches chats
+const searchMeassages=()=> {
+    const val=messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name= chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val)!=-1){
+            chat.style.display='flex';
+        }else{
+            chat.style.display= 'none';  
+        }
+ 
+    })                                                  
+}
+
+
+
+// highlight messages card when messages menu item is clicked
+messagesNotification.addEventListener('click',()=>{
+    messagesNotification.style.boxShadow='0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display='none';
+    setTimeout(()=>{    
+        messages.style.boxShadow='none';
+    },2000);
+})
+
+
+/// THEME CUSTOMIZATION
+
+// opens model
+const openThemeModal=()=>{
+   themeModal.style.display='grid';
+}
+
+// closes modal
+const closeThemeModal=(e)=>{
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display='none';
+    }
+}
+
+
+
+// close modal
+themeModel.addEventListener('click',closeThemeModal);
+
+theme.addEventListener('click',openThemeModal);
+
+// ======================== FONTS ==========================
+fontSizes.forEach(size => {
+    let fontSize;
+
+    size.addEventListener('click',()=>{
+        if(size.classList.contains('font-size-1')){
+            fontSize='10px';
+            root.style.setProperty('--sticky-top-left','5.4rem');
+            root.style.setProperty('--sticky-top-right','5.4rem');
+        } else if(size.classList.contains('font-size-2')){
+            fontSize='13px';
+            root.style.setProperty('--sticky-top-left','5.4rem');
+            root.style.setProperty('--sticky-top-right','-7rem');
+        } else if(size.classList.contains('font-size-3')){
+            fontSize='16px';
+            root.style.setProperty('--sticky-top-left','-2rem');
+            root.style.setProperty('--sticky-top-right','-17rem');
+        } else if(size.classList.contains('font-size-4')){
+            fontSize='19px';
+            root.style.setProperty('--sticky-top-left','-5rem');
+            root.style.setProperty('--sticky-top-right','-25rem');
+        } else if(size.classList.contains('font-size-5')){
+            fontSize='22px';
+            root.style.setProperty('--sticky-top-left','-12rem');
+            root.style.setProperty('--sticky-top-right','-35rem');
+        }
+
+        // change font size of the root html element
+        document.querySelector('html').style.fontSize=fontSize;
+    })
+})
+// remove active class from colors
+const changeActiveColorClass=()=>{
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+// change primary colors
+colorPalette.forEach(color=>{
+    color.addEventListener('click',()=>{
+        let primary;
+        if(color.classList.contains('color-1')){
+            primaryHue=252;
+        } else if(color.classList.contains('color-2')){
+            primaryHue=52; 
+        } else if(color.classList.contains('color-3')){
+            primaryHue=352;
+        } else if(color.classList.contains('color-4')){
+            primaryHue=152;
+        } else if(color.classList.contains('color-5')){
+            primaryHue=202;
+        }
+        color.classList.add('active');
+
+        root.style.setProperty('--primary-color-hue',primaryHue);
+
+    })
+})
+
+//theme BACKGROUND values
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+
+Bg2.addEventListener('click',()=>{
+    darkColorLightness='95%';
+    whiteColorLightness='20%';
+    lightColorLightness='15%';
+
+    // add active class
+    Bg2.classList.add('active');
+    // remove active class from the others
+    Bg1.classList.remove('active');
+    bg3.classList.remove('active');
+    changeBG();
+});
